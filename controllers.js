5924100f99257dda25f15b2e0e0f9b43b647b555
Vollmondo <dmitrifom74@gmail.com>
2023-07-12 13:22:20 +0500

    made some directories

--- controllers.js
+++
@@ -1,6 +0,0 @@
-// Работаем с leaflet. Документация: https://mourner.github.io/Leaflet/reference.html
-
-//Добавляем контроллер для отображения масштаба карты
-L.control.scale({
-    position:'bottomleft'
-    }).addTo(map);
